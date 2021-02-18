0c53b9bb284550506b706e675efd85828e26b519
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-18 11:28:11 +0300

    fix languages and DEBUG

--- auth/settings.py
+++ auth/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
