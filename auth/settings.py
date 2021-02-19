cd694d8f996ecafa945501c57882fdc0b9dc587c
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-19 12:32:06 +0300

    * change static endpoint from /static/ to /auth/static

--- auth/settings.py
+++ auth/settings.py
@@ -126,7 +126,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/auth/static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
 
