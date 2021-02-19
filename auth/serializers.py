9f8f06140ccac684d03e66de8362f5c3ae0c8fa1
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-19 11:50:01 +0300

    * register and user serializer

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,8 +1,56 @@
-from rest_framework.serializers import HyperlinkedModelSerializer
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.serializers import (
+    ModelSerializer,
+    EmailField,
+    CharField,
+    ValidationError
+)
+from rest_framework.validators import (
+    UniqueValidator
+)
 
 
-class UserSerializer(HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['id', 'username', 'email', 'date_joined']
+
+
+class RegisterSerializer(ModelSerializer):
+    email = EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+
+    password = CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password]
+    )
+    password2 = CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'password2')
+        extra_kwargs = {
+            'first_name': {'required': False},
+            'last_name': {'required': False}
+        }
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['email'],
+            email=validated_data['email']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
