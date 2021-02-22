de9710cbac07d0114e798d04e9cbb194e6a6b9c2
Ilia Dmitriev <ilia.dmitriev@gmail.com>
2021-02-22 11:55:15 +0300

    added custom fields to jwt payload: username and scope:admin

--- auth/urls.py
+++ auth/urls.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from .viewsets import UserView, RegisterView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from .viewsets import UserView, RegisterView, ScopeTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('auth/api/', include('rest_framework.urls'), name='auth_login'),
     path('auth/user/', UserView.as_view(), name='user_detail'),
     path('auth/admin/', admin.site.urls, name='admin'),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/', ScopeTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/register/', RegisterView.as_view(), name='register'),
 ]
--- auth/viewsets.py
+++ auth/viewsets.py
@@ -1,9 +1,13 @@
+from abc import ABC
+
 from rest_framework.generics import CreateAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer, RegisterSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 # ViewSets define the view behavior.
@@ -19,3 +23,20 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
+
+
+class ScopeTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        if user.is_staff or user.is_superuser:
+            token['scope'] = 'admin'
+
+        return token
+
+
+class ScopeTokenObtainPairView(TokenObtainPairView):
+    serializer_class = ScopeTokenObtainPairSerializer
+
